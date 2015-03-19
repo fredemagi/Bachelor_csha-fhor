135ad66a6864b9c557dae57a14638abb87464499
fredemagi <fredemagi@hotmail.com>
2015-03-19 14:19:10 +0100

    Første udgave GUI

--- Voting System/Voting System/MainWindow.xaml.cs
+++ Voting System/Voting System/MainWindow.xaml.cs
@@ -11,5 +11,10 @@ namespace Voting_System
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
