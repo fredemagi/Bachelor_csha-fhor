a5b3bf0cccc7fc73f018d8c219c85411928b2c64
fredemagi <fredemagi@hotmail.com>
2015-03-19 11:55:45 +0100

    Tilføjet nyt VS-projekt

---
+++ Voting System/Voting System/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Voting_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
