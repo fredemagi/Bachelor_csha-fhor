c7f2d57757ed642ec8f4b9930c69882811590f76
cshansen <schylle2740@gmail.com>
2015-04-17 21:10:26 +0200

    printer

--- Voting System/Voting System/Form1.cs
+++ Voting System/Voting System/Form1.cs
@@ -15,6 +15,14 @@ namespace Voting_System
         public Form1()
         {
             InitializeComponent();
+
+            Printer printer = new Printer();
+            printer.SetOutput("Helle Thorning Schmidt [ A ]");
+            printer.Print();
+
+            Image qr = Image.FromFile("C:/Users/SATLP850132/Pictures/336739.jpg");
+            printer.SetQR(qr);
+            printer.PrintQR();
         }
     }
 }
--- Voting System/Voting System/Printer.cs
+++ Voting System/Voting System/Printer.cs
@@ -4,9 +4,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Drawing.Printing;
+
+using System.Windows.Forms;
+using System.Drawing;
+
 namespace Voting_System
 {
     public class Printer
     {
+        // adjust picture to paper
+        // print QR code
+        // print vote
+
+        private Image _QRcode;
+        private string _output;
+        private string _printer;
+        private PaperSize _size = new PaperSize("Custom", 800, 200);
+        private PaperSize _sizeQR = new PaperSize("Custom QR", 2200, 1200);
+
+        public void SetQR(Image QRcode)
+        {
+            _QRcode = QRcode;
+        }
+
+        public void SetOutput(string output)
+        {
+            _output = output;
+        }
+
+        public void SetPrinter(string printer)
+        {
+            _printer = printer;
+        }
+
+        private Image BitmapToImage(Bitmap bmp)
+        {
+            Image img = (Image)bmp;
+            return img;
+        }
+
+        public void Print()
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.DocumentName = "Ballot Marker Vote Print";
+            pd.DefaultPageSettings.PaperSize = _size;
+
+            pd.PrintPage += new PrintPageEventHandler(PrintVoteEvent);
+
+            PrintPreviewDialog preview = new PrintPreviewDialog();
+            preview.Document = pd;
+            preview.ShowDialog();
+
+            //pd.PrinterSettings.PrinterName = printer;
+            //pd.Print();
+        }
+
+        public void PrintQR()
+        {
+            PrintDocument pd = new PrintDocument();
+            pd.DocumentName = "QR Code Print";
+            pd.DefaultPageSettings.PaperSize = _sizeQR;
+
+            pd.PrintPage += new PrintPageEventHandler(PrintQREvent);
+
+            PrintPreviewDialog preview = new PrintPreviewDialog();
+            preview.Document = pd;
+            preview.ShowDialog();
+        }
+
+        private void PrintVoteEvent(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(_output, new Font("Courier New", 32), Brushes.Black, 20, 20);
+        }
+
+        private void PrintQREvent(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(_QRcode, new Point(10,10));
+        }
     }
 }
